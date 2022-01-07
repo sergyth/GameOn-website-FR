4df5e8a68baea052a27feda7bc50d107e9eae9f2
sergyth <73670477+sergyth@users.noreply.github.com>
2022-01-07 12:52:22 +0100

    validation du formulaire(incomplet)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -22,9 +22,83 @@ function launchModal() {
 
 //modal closure
 
-const closeModalBtn =document.querySelector('.close');
-closeModalBtn.addEventListener('click', closeModal);
-function closeModal(){
+const closeModalBtn = document.querySelector(".close");
+closeModalBtn.addEventListener("click", closeModal);
+function closeModal() {
   modalbg.style.display = "none";
 }
+
+//validation of firstname
+let inputFirstName = document.getElementById("first");
+inputFirstName.addEventListener("input", isFirstNameValid);
+
+function isFirstNameValid() {
+  const parent = inputFirstName.closest(".formData");
+  if (inputFirstName.value.length < 2) {
+    parent.setAttribute("data-error-visible", "true");
+  } else {
+    parent.setAttribute("data-error-visible", "false");
+  }
+}
+
+//validation of lastname
+let inputLastName = document.getElementById("last");
+inputLastName.addEventListener("input", isLastNameValid);
+
+function isLastNameValid() {
+  const parent = inputLastName.closest(".formData");
+  if (inputLastName.value.length < 2) {
+    parent.setAttribute("data-error-visible", "true");
+  } else {
+    parent.setAttribute("data-error-visible", "false");
+  }
+}
+
+// validation email
+
+let email = document.getElementById("email");
+
+email.addEventListener("input", emailIsValid);
+
+function emailIsValid() {
+  const parent = email.closest(".formData");
+  if (email.validity.typeMismatch) {
+    parent.setAttribute("data-error-visible", "true");
+  } else {
+    parent.setAttribute("data-error-visible", "false");
+  }
+}
+
+
+
+//validation of number of tournament
+
+let numberOfTournament = document.getElementById("quantity");
+numberOfTournament.addEventListener('input', isANumber);
+
+function isANumber (){
+  const parent = numberOfTournament.closest('.formData');
+  Number(numberOfTournament.value);
+  if(isNaN(numberOfTournament.value)){
+    parent.setAttribute("data-error-visible", "true");
+  }else{
+    parent.setAttribute("data-error-visible", "false");
+  }
+}
+
+
+//validation of town
+
+let locationInput = document.reserve.location;
+console.log(locationInput);
+for(let i = 0; i < locationInput.length; i++){
+  if(locationInput[i].checked){
+    console.log('checked');
+    break
+  }else{
+  console.log('unchecked')
+  }
+  
+}
+
 
